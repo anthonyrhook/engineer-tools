f2622f8bd39c5a88f3e2d1cf4b84f722e7e03098
Anthony Hook <anthony.hook3@gmail.com>
2015-12-15 11:39:38 -0600

    Create disk-cleanup-no-reboot-32
    Disk cleanup on a 2008 R2 32bit box without restarting

---
+++ disk-cleanup-no-reboot-32.bat
@@ -0,0 +1,3 @@
+copy C:\Windows\winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.0.6001.18000_none_c962d1e515e94269\cleanmgr.exe C:\Windows\System32\
+copy C:\Windows\winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.0.6001.18000_en-us_b9f50b71510436f2\cleanmgr.exe.mui C:\Windows\System32\en-US\
+cleanmgr.exe
