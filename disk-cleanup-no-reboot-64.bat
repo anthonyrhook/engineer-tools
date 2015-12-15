f0196a5a1076ec6d90c9c40c692db1a91f4f0fff
Anthony Hook <anthony.hook3@gmail.com>
2015-12-15 11:38:32 -0600

    Create disk-cleanup-no-reboot-64
    Batch file to enable 'disk cleanup' utility on a 2008 R2 64bit server without rebooting

---
+++ disk-cleanup-no-reboot-64.bat
@@ -0,0 +1,3 @@
+ copy C:\Windows\winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe C:\Windows\System32\
+ copy C:\Windows\winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui C:\Windows\System32\en-US\
+ cleanmgr.exe
